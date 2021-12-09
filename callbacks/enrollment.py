4454d69eab9b8c68135fe06db002aab636a47622
Jun Kai <ljlimjk10@gmail.com>
2021-12-09 16:42:06 +0800

    Fixed bugs

--- callbacks/enrollment.py
+++ callbacks/enrollment.py
@@ -26,7 +26,8 @@ def enrollment_get_info_callback(update: Update, context: CallbackContext) -> No
     event_instance_code = update.message.text
     event_instance, status_code = ApiService.get_specific_event_instance(
         event_instance_code, context)
-    if status_code == HTTPStatus.OK and event_instance.get(EventInstance.IS_COMPLETED):
+    # if status_code == HTTPStatus.OK and event_instance.get(EventInstance.IS_COMPLETED):
+    if status_code == HTTPStatus.OK:        
 
         context.user_data[Enrollment.ENROLLMENT_DATA] = {
             Enrollment.USERNAME: TelegramService.get_user_id(update),
@@ -103,22 +104,18 @@ def enrollment_submit_info_callback(update: Update, context: CallbackContext) ->
     event_instance_code = enrollment_data.get(EventInstance.CODE)
     enrollment_exist = ApiService.check_enrollment_exist(
         user_id, event_instance_code, context)
-
     if not enrollment_exist:
+        # if float(enrollment_data["fee"]) == 0:
+        enrollment, status_code = ApiService.create_enrollment(payload, context)
+        if status_code == HTTPStatus.CREATED:
+            msg = "Enrolled to course, returning to main menu...."
+            context.user_data[State.START_OVER.value] = True
+            TelegramService.reply_text(msg, update)
+            start.start_callback(update, context)
+            return State.BACK.value
 
-        if float(enrollment_data["fee"]) == 0:
-
-            _, status_code = ApiService.create_enrollment(payload, context)
-
-            if status_code == HTTPStatus.CREATED:
-                msg = "Enrolled to course, returning to main menu...."
-                context.user_data[State.START_OVER.value] = True
-                TelegramService.reply_text(msg, update)
-                start.start_callback(update, context)
-                return State.BACK.value
-
-            else:
-                BackendError(details=status_code)
+        else:
+            BackendError(details=status_code)
 
     else:
         msg = "You are already enrolled, returning to main menu...."
